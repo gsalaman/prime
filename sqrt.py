af24e96b7cde8227175d956f74cc54fd82023afb
Glenn Salaman <43499190+gsalaman@users.noreply.github.com>
2023-01-20 15:40:20 -0700

    sqrt version

--- sqrt.py
+++ sqrt.py
@@ -1,4 +1,5 @@
-# Initial algo...just counting up
+# Next improvement: stop at sqrt.
+from math import sqrt, floor
 
 # prime number definition...only divisibly by 1 and itself. 
 
@@ -7,14 +8,16 @@ top_candidate = 100
 
 prime_set = []
 
+# next version:  check factors from 2 up to the square root of our candidate.  
 for candidate in range(2,top_candidate):
-  # brute force version:  check factors from 2 up to our candidate.  
-  
+
   # Start by assuming the number is prime
   prime = True
- 
-  # check the factors from 2 up to (but not including) our candidate number
-  for factor in range (2, candidate):
+
+  top_factor = floor(sqrt(candidate))
+
+  # check the factors from 2 up to and including the sqrt of our candidate 
+  for factor in range (2, top_factor+1):
     if (candidate % factor == 0):
       prime = False
       break;
